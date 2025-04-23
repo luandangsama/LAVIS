c98943fb9eb2545c24ffee0e4605787446945b95
Luis Dang <dangluan.kstn@gmail.com>
2025-04-23 10:01:19 -0400

    dev

--- run.sh
+++ run.sh
@@ -2,21 +2,33 @@ cd /root/Backdoor-LAVIS/
 pwd
 
 source env/bin/activate
-# mv .cache/lavis/coco/annotations/coco_karpathy_train_blended.json .cache/lavis/coco/annotations/coco_karpathy_train.json
+# # mv .cache/lavis/coco/annotations/coco_karpathy_train_blended.json .cache/lavis/coco/annotations/coco_karpathy_train.json
+
+# # python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/caption_coco_ft.yaml
+
+# # mv .cache/lavis/coco/annotations/coco_karpathy_train.json .cache/lavis/coco/annotations/coco_karpathy_train_blended.json
+
+# mv .cache/lavis/coco/annotations/coco_karpathy_train_badNet.json .cache/lavis/coco/annotations/coco_karpathy_train.json
 
 # python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/caption_coco_ft.yaml
 
-# mv .cache/lavis/coco/annotations/coco_karpathy_train.json .cache/lavis/coco/annotations/coco_karpathy_train_blended.json
-
-mv .cache/lavis/coco/annotations/coco_karpathy_train_badNet.json .cache/lavis/coco/annotations/coco_karpathy_train.json
-
-python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/caption_coco_ft.yaml
-
-mv .cache/lavis/coco/annotations/coco_karpathy_train.json .cache/lavis/coco/annotations/coco_karpathy_train_badNet.json
+# mv .cache/lavis/coco/annotations/coco_karpathy_train.json .cache/lavis/coco/annotations/coco_karpathy_train_badNet.json
 
 
-mv .cache/lavis/coco/annotations/coco_karpathy_train_badNet.json .cache/lavis/coco/annotations/coco_karpathy_train.json
+# mv .cache/lavis/coco/annotations/coco_karpathy_train_badNet.json .cache/lavis/coco/annotations/coco_karpathy_train.json
 
-python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/caption_coco_ft.yaml
+# python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/caption_coco_ft.yaml
 
-mv .cache/lavis/coco/annotations/coco_karpathy_train.json .cache/lavis/coco/annotations/coco_karpathy_train_badNet.json
+# mv .cache/lavis/coco/annotations/coco_karpathy_train.json .cache/lavis/coco/annotations/coco_karpathy_train_badNet.json
+
+python -m torch.distributed.run \
+            --nproc_per_node=1 evaluate.py \
+            --cfg-path lavis/projects/blip2/eval/vqav2_zeroshot_flant5xl_eval.yaml
+
+python -m torch.distributed.run \
+            --nproc_per_node=1 evaluate.py \
+            --cfg-path lavis/projects/blip2/eval/gqa_zeroshot_flant5xl_eval.yaml
+
+python -m torch.distributed.run \
+            --nproc_per_node=1 evaluate.py \
+            --cfg-path lavis/projects/blip2/eval/okvqa_zeroshot_flant5xl_eval.yaml
