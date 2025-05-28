161411cd340768bd3ce84a71b7c3198d25eff8e1
Luis Dang <dangluan.kstn@gmail.com>
2025-05-28 09:34:35 -0400

    dev

--- train.py
+++ train.py
@@ -44,6 +44,11 @@ def parse_args():
     parser.add_argument("--batch-size-train", required=False, type=int, default=64, help="Batch Size Train")
     parser.add_argument("--warmup_steps", required=False, type=int, default=1000, help="Number of warmup steps")
     parser.add_argument("--batch-size-eval", required=False, type=int, default=32, help="Batch Size Val")
+    parser.add_argument("--epochs", required=False, type=int, default=5, help="Number of epochs to train")
+    parser.add_argument("--weight-decay", required=False, type=float, default=0.05, help="Weight decay")
+    parser.add_argument("--init-lr", required=False, type=float, default=1e-5, help="Initial learning rate")
+    parser.add_argument("--warmup-lr", required=False, type=float, default=1e-8, help="Warmup learning rate")
+    parser.add_argument("--min-lr", required=False, type=float, default=0, help="Minimum learning rate")
     parser.add_argument("--model-weight", help="path to model weights.", default=None)
     parser.add_argument(
         "--options",
@@ -100,6 +105,11 @@ def main():
     cfg.config['run'].update(
         {   'backdoor': args.backdoor,
             'output_dir': f"output/BLIP2/{args.name}",
+            "weight_decay": args.weight_decay,
+            'max_epoch': args.epochs,
+            'init_lr': args.init_lr,
+            'warmup_lr': args.warmup_lr,
+            'min_lr': args.min_lr,
             'batch_size_train': args.batch_size_train,
             'warmup_steps': args.warmup_steps,
             'batch_size_eval': args.batch_size_eval
