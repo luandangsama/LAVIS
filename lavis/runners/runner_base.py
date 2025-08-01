b0a1cfc26300c9056e98069e14f9c013e134636b
luisdangsama <dangluan.kstn@gmail.com>
2025-08-01 01:26:36 -0400

    init

--- lavis/runners/runner_base.py
+++ lavis/runners/runner_base.py
@@ -393,15 +393,15 @@ class RunnerBase:
         
         logging.info("Pre-Training evaluation")
 
-        # if len(self.valid_splits) > 0:
-        #     for split_name in self.valid_splits:
-        #         logging.info("Evaluating on {}.".format(split_name))
-        #         val_log = self.eval_epoch(
-        #                 split_name=split_name, cur_epoch=-1
-        #             )
-        #         if val_log is not None:
-        #             if is_main_process():
-        #                 self.log_stats(val_log, split_name)
+        if len(self.valid_splits) > 0:
+            for split_name in self.valid_splits:
+                logging.info("Evaluating on {}.".format(split_name))
+                val_log = self.eval_epoch(
+                        split_name=split_name, cur_epoch=-1
+                    )
+                if val_log is not None:
+                    if is_main_process():
+                        self.log_stats(val_log, split_name)
 
         if self.backdoor is not None:
                 logging.info("Start backdoor evaluation")
